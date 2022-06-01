247e785ab18ab63740faf40478413778c38a15a4
CFeeney333 <c.feeney333@gmail.com>
2022-06-01 11:08:28 +0100

    Move grid creation to a function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -7,14 +7,16 @@ container.className = "grid-container";
 container.style.gridTemplateColumns = GRID_SIZE;
 container.style.gridTemplateRows = "auto";
 
-for (let i = 1; i <= GRID_SIZE; i++) {
-  for (let j = 1; j <= GRID_SIZE; j++) {
-    const item = document.createElement("div");
-    item.className = "grid-item";
-    item.style.gridColumn = "" + i + "/" + (i + 1);
-    item.style.gridRow = "" + j + "/" + (j + 1);
-    item.addEventListener("mouseenter", onHover);
-    container.appendChild(item);
+function createGrid(size) {
+  for (let i = 1; i <= size; i++) {
+    for (let j = 1; j <= size; j++) {
+      const item = document.createElement("div");
+      item.className = "grid-item";
+      item.style.gridColumn = "" + i + "/" + (i + 1);
+      item.style.gridRow = "" + j + "/" + (j + 1);
+      item.addEventListener("mouseenter", onHover);
+      container.appendChild(item);
+    }
   }
 }
 
